69f8f974ecebca523b791c1c0451da9154f08257
Xealia <lillylar93@outlook.com>
2018-07-26 11:39:18 +0200

    Added and changed

--- LiarsDiceGameboard/LiarsDiceGameboard/js/DieView.js
+++ LiarsDiceGameboard/LiarsDiceGameboard/js/DieView.js
@@ -1,1 +1,24 @@
-﻿
+﻿class DieView {
+    constructor() {
+        this.number = DieModel.number;
+    }
+    GetDieFace() {
+        switch (this.number) {
+            case 1:
+                return img.src = "dice-face-1.png";
+            case 2:
+                return img.src = "dice-face-2.png";
+            case 3:
+                return img.src = "dice-face-3.png";
+            case 4:
+                return img.src = "dice-face-4.png";
+            case 5:
+                return img.src = "dice-face-5.png";
+            case 6:
+                return img.src = "dice-face-6.png";
+
+            default:
+                return null;
+        }
+    }
+}
